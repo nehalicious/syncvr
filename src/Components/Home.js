3e544cdab8690bd756ce66c8d878b2c4f75157e7
nkalia1802 <n.kalia@student.tudelft.nl>
2021-04-06 17:40:34 +0200

    Getting data from firestore works

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,17 +1,26 @@
 import React, {useState} from 'react';
 import {Container, Row, Form, Button} from 'react-bootstrap'
+import {db} from '../firebase'
 
 export default function Home() {
     const [number, setNumber] = useState(1);
     const [error, displayError] = useState(false)
 
-    const handleSubmit = () => {
-
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(number);
+        // or get the single doc from the collection
+        db.collection("fibonacci")
+            .doc(number)
+            .get()
+            .then(doc => {
+                const data = doc.data();
+                console.log(data);
+            });
     };
 
     const handleChange = (input) => {
         displayError(false);
-        console.log(input);
         if(input>0) {
             setNumber(input);
         } else displayError(true);
@@ -27,7 +36,7 @@ export default function Home() {
                         <Form.Control onChange={e=>handleChange(e.target.value)} type="number" placeholder="Enter number" />
                     </Form.Group>
 
-                    <Button variant="primary" type="submit">
+                    <Button onClick = {(e)=>handleSubmit(e)}variant="primary" type="submit">
                         Submit
                     </Button>
                     {error? <Form.Text> Please enter a positive number </Form.Text> : null}

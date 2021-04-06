3737cbe0bceea9d0af38967e25ca51a1da1c5ba0
nkalia1802 <n.kalia@student.tudelft.nl>
2021-04-06 23:20:25 +0200

    Fix error messages

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,21 +11,24 @@ import axios from 'axios';
 export default function Home() {
     const [number, setNumber] = useState(1);
     const [error, displayError] = useState(false);
+    const [serverError, displayServerError] = useState(false);
     const history = useHistory();
+    const [result, setResult] = useState(null);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         await axios.post('https://us-central1-syncvr-fc5d5.cloudfunctions.net/app/fibonacci/', {
             index: number
         }).then(res=>{
-            if(res.status === 200) {console.log(res.data)}
-            else{console.log(error)}
+            if(res.status === 200) {setResult(res.data)}
+            else{displayServerError(true)}
             }
-        ).catch(e=>console.log(error))
+        ).catch(e=>displayServerError(true))
     };
 
     const handleChange = (input) => {
         displayError(false);
+        displayServerError(false);
         if(input>0) {
             setNumber(input);
         } else displayError(true);
@@ -60,6 +63,9 @@ export default function Home() {
                     {error? <Form.Text> Please enter a positive number </Form.Text> : null}
                 </Form>
             </Row>
+            {result ? <Row> The fibonacci number is: {result.value} </Row>:null}
+             {serverError ? <Row> We wouldn't get the server to worK properly :( You can still check the previous requests though </Row>:null}
+
             <Row style={{width: '100%'}} className="p-5">
                 <Button
                     onClick={()=>{history.push('/OldRequests')}}>
